922d376a6686a6cabee1d554a0ed5c4c9b125f31
lujiawei <ljw18721327472@outlook.com>
2021-02-16 11:22:23 +0000

    2021-2-1 optimize gitbook structure

---
+++ book.js
@@ -0,0 +1,15 @@
+{
+    "plugins": [
+        "advanced-emoji",
+        "katex",
+        "mermaid-gb3",
+        "include-codeblock"
+    ],
+    "pluginsConfig": {
+        "include-codeblock": {
+            "template": "ace",
+            "unindent": "true",
+            "theme": "monokai"
+        }
+    }
+}
--- markdown/01-advanced/README.md
+++ markdown/01-advanced/README.md
@@ -3,3 +3,5 @@
 * [HTML](/markdown/01-advanced/01-html.md)
 * [公式](/markdown/01-advanced/02-公式.md)
 * [转义](/markdown/01-advanced/03-转义.md)
+* [表情符号](/markdown/01-advanced/04-表情.md)
+* [排版优化](/markdown/01-advanced/05-排版.md)
